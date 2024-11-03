09a013ad9959d7dcfb34f13e0ba9cf3e65219514
buyakashaPIZZA <buyakasha.pizza@gmail.com>
2024-11-03 22:22:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-bluelock_url = "https://www.animesrbija.com/anime/blue-lock-vs-u-20-japan"
+bluelock_url = "https://aniwatchtv.to/watch/blue-lock-season-2-19318?ep=128447"
 
 # get website contents and parse html
 response = requests.get(bluelock_url)
 html = BeautifulSoup(response.text, 'html.parser')
 
-newText = html.find('span', {"class" : "anime-episode-num"})
+newText = html.find('div', {"id" : "detail-ss-list"})
 
 # create markdown string of courses
 novosti_markdown = newText.text
